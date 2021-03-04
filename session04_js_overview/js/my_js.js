9135a2dedea68c39c174c25db1af2908b431f161
Trung Doan <duytrung177@gmail.com>
2021-03-04 10:03:58 +0700

    session 04

---
+++ session04_js_overview/js/my_js.js
@@ -0,0 +1,20 @@
+// alert("Đây là đoạn thông tin hiển thị từ file JS riêng");
+// alert("Đây là alert()");
+// confirm("Đây là confirm");
+// prompt("Đây là prompt");
+for (let i = 0; i < 100; i++){
+    console.log(i);
+}
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+console.log("Đây là console.log");
+document.writeln("Dòng thứ 1");
+document.writeln("<br>");
+document.writeln("Dòng thứ 2");
+// document.getElementById("result").innerText = "Đây là innerText <br>";
+document.getElementById("result").innerHTML = "Đây là innerHTML <a href='https://google.com'>Google</a>";
